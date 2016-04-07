c49b410af74f4ba3f18060b2700d9aa8926472b7
Anastasiya Khorevich <a.khorevich@bssys.com>
2016-04-07 18:06:07 +0300

    no message

--- bulls'n'cows/src/view/ConsoleView.java
+++ bulls'n'cows/src/view/ConsoleView.java
@@ -12,19 +12,26 @@ import java.util.Scanner;
 public class ConsoleView {
 
     String num;
+    String name;
     private final static Scanner IN = new Scanner(System.in);
 
     public void gameStart(){
-
+        System.out.print("Введите свое имя: ");
+        name = IN.nextLine();
+        Player player = new Player();
+        PlayerController pl = new PlayerController(player,true);
+        player.setName(name);
         try{
             System.out.print("Введите число: ");
             num = IN.nextLine();
+            player.setNumber(num);
             String[] number = num.split("");
             int[] intArray = new int[number.length];
             for (int i = 0; i < number.length; i++) {
                 String numberAsString = number[i];
                 intArray[i] = Integer.parseInt(numberAsString);
             }
+            System.out.println(player.getNumber());
         }catch (final InputMismatchException e){
             System.out.println("Это не число");
 
