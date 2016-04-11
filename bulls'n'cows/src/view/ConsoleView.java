173be917ec5db09b589fa8fd3b9e0e90f971ec3b
Nancy <a.khorevich@gmail.com>
2016-04-11 07:35:51 +0300

    new

--- bulls'n'cows/src/view/ConsoleView.java
+++ bulls'n'cows/src/view/ConsoleView.java
@@ -3,62 +3,98 @@ package view;
 import controller.PlayerController;
 import model.Player;
 
-import java.util.ArrayList;
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.*;
 
-/**
- * Created by khoAK on 06.04.2016.
- */
+
 public class ConsoleView {
 
     String num;
     String name;
-    ArrayList<Integer> intArray;
+    String[] enemyNum;
     Player player;
     PlayerController plCont;
     private final static Scanner IN = new Scanner(System.in);
 
-    public void gameStart(){
+    public void gameStart() {
         player = new Player();
         setName();
-        if (name==null){
-            System.out.println("Имя не может быть пустым");
-            setName();
-        }else {
-            player.setName(name);
+
+
+
+
+        plCont = new PlayerController(player, true);
+        enemyNumber();
+
+
+
+
+    }
+
+    public void enemyNumber() {
+        enemyNum = new String[4];
+        Random r = new Random();
+
+        for (int i = 0; i < enemyNum.length; i++) {
+            enemyNum[i] = String.valueOf(r.nextInt(11));
+        }
+        boolean checkEn = checkDupl(enemyNum);
+        if (checkEn==true){
+            enemyNumber();
+        } else {
+            setNum();
         }
 
-
-        plCont = new PlayerController(player,true);
-
-
+        //System.out.println(Arrays.toString(enemyNum));
     }
-    public void setName(){
+
+    public void setName() {
         System.out.print("Введите свое имя: ");
         name = IN.nextLine();
+        if (name == null) {
+            System.out.println("Имя не может быть пустым");
+            setName();
+
+        } else {
+            player.setName(name);
+        }
     }
-    public void setNum(){
+
+    public void setNum() {
         if (plCont.isTurn()) {
             try {
                 System.out.print("Введите число: ");
                 num = IN.nextLine();
-                player.setNumber(num);
-                String[] number = num.split( "(?<=(.))(?!\\1)" );
-                intArray = new ArrayList<>(number.length);
-                for (int i = 0; i < number.length; i++) {
-                    String numberAsString = number[i];
-                    intArray.add(Integer.parseInt(numberAsString));
+
+                String[] number = num.split("");
+                //System.out.println(Arrays.toString(number));
+                boolean check = checkDupl(number);
+                if (check == true) {
+                    System.out.println("Нельзя вводить повторяющиеся числа!");
+                    setNum();
+                } else {
+                    System.out.println("Все окей");
                 }
-                plCont.setTurn(false);
-                System.out.println("Игрок "+player.getName()+" загадал число "+player.getNumber());
+
+
             } catch (final InputMismatchException e) {
                 System.out.println("Это не число");
 
             }
+        } else {
+            System.out.println("Не ваш ход!");
         }
-        
+
     }
 
+    public static boolean checkDupl(String[] input) {
+        for (int i = 0; i < input.length; i++) {
+            for (int j = 0; j < input.length; j++) {
+                if (input[i].equals(input[j]) && i != j) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
 }
